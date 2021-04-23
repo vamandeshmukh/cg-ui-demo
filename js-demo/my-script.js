a3ca88615fd0b09cd9b23ed9e3dfd768f7c4f6e8
vamandeshmukh <vamandeshmukh@gmail.com>
2021-04-23 10:09:27 +0530

    Commit
    COmmit

---
+++ js-demo/my-script.js
@@ -0,0 +1,20 @@
+// my-script.js
+
+
+function fun() {
+
+    document.getElementById("some").innerHTML = "Some other text";
+
+    num = 10;
+    console.log('1 from external Script. ' + num);
+    console.log("2 from external Script. " + num);
+    console.log(`3 from external Script. ${num}`);
+
+    // alert("You clicked a button!");
+}
+
+// fun();
+
+
+
+
